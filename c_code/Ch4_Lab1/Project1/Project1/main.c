7fa8065fc35fcc1cb6ee845d74785869335c30d5
zue4331 <71508905+zue4331@users.noreply.github.com>
2020-11-13 20:59:06 +0800

    Add files via upload

---
+++ c_code/Ch4_Lab1/Project1/Project1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#define SIZE 7
+
+int main(void) {
+	int face, roll, frequency[SIZE] = { 0 };
+	
+	srand(time(NULL));
+	for (roll = 1; roll <= 6000; roll++) {
+		face = 1 + (rand() % 6);
+		++frequency[face];
+	}
+
+	printf("%s%17s\n", "Face", "Frequency");
+	
+	for (face = 1; face < SIZE; face++) printf("%4d%17d\n", face, frequency[face]);
+
+	return 0;
+}
